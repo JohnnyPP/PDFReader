153c00ddfff4491c23bb8a2dd4edec40ba4a0ecf
Chris <chris.kolann@gmail.com>
2017-11-07 15:34:56 +0100

    Implemented read method

--- PDFReader/PDFReader/Program.cs
+++ PDFReader/PDFReader/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
 
 namespace PDFReader
 {
@@ -10,6 +12,20 @@ namespace PDFReader
 	{
 		static void Main(string[] args)
 		{
+			Console.Write(PdfText(@"D:\DTemp\test.pdf"));
+		}
+
+
+		public static string PdfText(string path)
+		{
+			PdfReader reader = new PdfReader(path);
+			string text = string.Empty;
+			for (int page = 1; page <= reader.NumberOfPages; page++)
+			{
+				text += PdfTextExtractor.GetTextFromPage(reader, page);
+			}
+			reader.Close();
+			return text;
 		}
 	}
 }
