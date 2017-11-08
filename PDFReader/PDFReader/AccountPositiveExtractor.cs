ba4a2ceaf61ae440cec4286ffcd0cf5700572a6b
Chris <chris.kolann@gmail.com>
2017-11-08 17:35:02 +0100

    Added new classes for positive and negative extraction

---
+++ PDFReader/PDFReader/AccountPositiveExtractor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDFReader
+{
+	/// <summary>
+	/// This class extracts all positive amounts from the account avoiding internal transfers
+	/// </summary>
+	class AccountPositiveExtractor
+	{
+	}
+}
